cb77527526770030a07c880c863d25e2c80975e4
michaelujkim <michaelujkim@gmail.com>
2018-03-13 16:11:12 -0700

    added fonts to assets and color hexcodes into a newly created /config/styles.js

---
+++ js/config/styles.js
@@ -0,0 +1,11 @@
+export const colors = {
+  black: "#232323",
+  nearBlack: "#333333",
+  Grey: "#EEEEEE",
+  green: "#92BFB5",
+  red: "#E17B61"
+};
+
+export const typography = {
+  fontMain: "Avenir"
+};
