dbbdef91f3b669d0b1f73528636688349378ab85
Sstobo <seanstobo@gmail.com>
2018-03-14 15:22:26 -0700

    Firebase helper functions in firebaseFunctions firebase.js

--- js/firebaseFunctions/firebase.js
+++ js/firebaseFunctions/firebase.js
@@ -1,69 +1,25 @@
-
-const firebase = require('firebase');
-require('firebase/firestore');
-import {FIREBASE_API_KEY} from "../config/constants.js"
-import {FIREBASE_AUTH_DOMAIN} from "../config/constants.js"
-import {FIREBASE_DATABASE_URL} from "../config/constants.js"
-import {FIREBASE_PROJECT_ID} from "../config/constants.js"
-import {FIREBASE_STORAGE_BUCKET} from "../config/constants.js"
+const usersRef = userID => database.doc('users/' + userID);
+const companysRef = database.doc('companys');
+const projectsRef = companyID => database.doc('companys/' + companyID + '/projects');
+const questionsRef = (companyID, projectNumber, questionNumber) =>
+	database.doc('companys/' + companyID + '/projects/' + projectNumber + '/questions/' + questionNumber);
 
 
-firebase.initializeApp({
-	apiKey: FIREBASE_API_KEY,
-	authDomain: FIREBASE_AUTH_DOMAIN,
-	projectId: FIREBASE_PROJECT_ID
+const bioText = "I was a coder then i blew it all up"
+
+usersRef.set({
+	bio: bioText
+}).then(function () {
+	console.log("Status saved");
+}).catch(function (error) {
+	console.log("Got an error", error)
 });
 
-var firestoreDB = firebase.firestore();
-
-const testGetUser = (hash) => firestoreDB
-	.collection('users')
-	.doc(hash)
-	.get()
-	.then(function(doc) {
-		if (doc.exists) {
-			let testData = doc.data();
-			console.log('!! Document data:', testData.bio);
-			return testData;
-		} else {
-			console.log('No such document!');
-		}
-	})
-	.catch(function(error) {
-		console.log('Error getting document:', error);
-	});
-
-  testGetUser('WJTvKQHAwP9frwE46woO');
-
-// Add to database function. Targetcollection is a string, either:
-// users.[idHash].field
-// companys.[idHash].field
-// companys.[idHash].projects.[project1,2,3].field
-// companys.[idHash].projects.[project1,2,3].questions.[question1,2,3].field
-
-function addToDB(targetCollection, toAdd) {
-
-firestoreDB.collection(targetCollection).add({toAdd
-})
-.then(function(docRef) {
-  console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-  console.error("Error adding document: ", error);
+usersRef(userID).get().then(function (doc) {
+	if (doc && doc.exists) {
+		const docData = doc.data();
+		console.log("Retrieved", docData)
+	}
+}).catch(function (error) {
+	console.log("Got an error", error)
 });
-}
-
-// Get field from database function. Targetcollection is a string, either:
-// users.[idHash].field
-// companys.[idHash].field
-// companys.[idHash].projects.[project1,2,3].field
-// companys.[idHash].projects.[project1,2,3].questions.[question1,2,3].field
-
-function getFromDB(targetCollection, toGet) {
-
-  firestoreDB.collection(targetCollection).get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-  });
-})
-}
