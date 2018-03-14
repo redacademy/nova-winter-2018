17dc0c68b1c518aff0e2bfedf805c3599dbe7e18
Sstobo <seanstobo@gmail.com>
2018-03-14 11:53:30 -0700

    Database initialized. Firebase helpers written. DB wiring confirmed live

--- js/firebaseFunctions/firebase.js
+++ js/firebaseFunctions/firebase.js
@@ -1,12 +1,10 @@
-
 const firebase = require('firebase');
 require('firebase/firestore');
-import {FIREBASE_API_KEY} from "../config/constants.js"
-import {FIREBASE_AUTH_DOMAIN} from "../config/constants.js"
-import {FIREBASE_DATABASE_URL} from "../config/constants.js"
-import {FIREBASE_PROJECT_ID} from "../config/constants.js"
-import {FIREBASE_STORAGE_BUCKET} from "../config/constants.js"
-
+import { FIREBASE_API_KEY } from '../config/constants.js';
+import { FIREBASE_AUTH_DOMAIN } from '../config/constants.js';
+import { FIREBASE_DATABASE_URL } from '../config/constants.js';
+import { FIREBASE_PROJECT_ID } from '../config/constants.js';
+import { FIREBASE_STORAGE_BUCKET } from '../config/constants.js';
 
 firebase.initializeApp({
 	apiKey: FIREBASE_API_KEY,
@@ -16,24 +14,25 @@ firebase.initializeApp({
 
 var firestoreDB = firebase.firestore();
 
-const testGetUser = (hash) => firestoreDB
-	.collection('users')
-	.doc(hash)
-	.get()
-	.then(function(doc) {
-		if (doc.exists) {
-			let testData = doc.data();
-			console.log('!! Document data:', testData.bio);
-			return testData;
-		} else {
-			console.log('No such document!');
-		}
-	})
-	.catch(function(error) {
-		console.log('Error getting document:', error);
-	});
+const testGetUser = hash =>
+	firestoreDB
+		.collection('users')
+		.doc(hash)
+		.get()
+		.then(function(doc) {
+			if (doc.exists) {
+				let testData = doc.data();
+				console.log('!! Document data:', testData.bio);
+				return testData;
+			} else {
+				console.log('No such document!');
+			}
+		})
+		.catch(function(error) {
+			console.log('Error getting document:', error);
+		});
 
-  testGetUser('WJTvKQHAwP9frwE46woO');
+testGetUser('WJTvKQHAwP9frwE46woO');
 
 // Add to database function. Targetcollection is a string, either:
 // users.[idHash].field
@@ -42,15 +41,17 @@ const testGetUser = (hash) => firestoreDB
 // companys.[idHash].projects.[project1,2,3].questions.[question1,2,3].field
 
 function addToDB(targetCollection, toAdd) {
-
-firestoreDB.collection(targetCollection).add({toAdd
-})
-.then(function(docRef) {
-  console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-  console.error("Error adding document: ", error);
-});
+	firestoreDB
+		.collection(targetCollection)
+		.add({
+			toAdd
+		})
+		.then(function(docRef) {
+			console.log('Document written with ID: ', docRef.id);
+		})
+		.catch(function(error) {
+			console.error('Error adding document: ', error);
+		});
 }
 
 // Get field from database function. Targetcollection is a string, either:
@@ -60,10 +61,12 @@ firestoreDB.collection(targetCollection).add({toAdd
 // companys.[idHash].projects.[project1,2,3].questions.[question1,2,3].field
 
 function getFromDB(targetCollection, toGet) {
-
-  firestoreDB.collection(targetCollection).get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-  });
-})
+	firestoreDB
+		.collection(targetCollection)
+		.get()
+		.then(querySnapshot => {
+			querySnapshot.forEach(doc => {
+				console.log(`${doc.id} => ${doc.data()}`);
+			});
+		});
 }
--- js/scenes/Login/LoginContainer.js
+++ js/scenes/Login/LoginContainer.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Login from './Login';
 import { testGetUser } from '../../api/firebaseHelpers';
 
+import { testGetUser } from '../../api/firebaseHelpers';
+import { getUsers } from '../../firebaseFunctions/firebase';
 class LoginContainer extends Component {
 	static route = {
 		navigationBar: {
