9f7cc8f2fa3e8f77f957ed380abf6c65a3fc3e58
Sstobo <seanstobo@gmail.com>
2018-03-14 11:53:30 -0700

    Firebase server initialized

---
+++ js/config/constants.js
@@ -0,0 +1,5 @@
+export const FIREBASE_API_KEY = "AIzaSyAtCr2ZpbWvFsl_g_Sco46VlLPCE21OFMs";
+export const FIREBASE_AUTH_DOMAIN = "nova-2018.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://nova-2018.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "nova-2018";
+export const FIREBASE_STORAGE_BUCKET = "nova-2018.appspot.com";
