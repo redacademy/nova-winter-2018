38da8e9c12088321d52e0fd77576344fdcaeae61
michaelujkim <michaelujkim@gmail.com>
2018-03-15 13:37:22 -0700

    login page set up done

--- js/scenes/Login/Login.js
+++ js/scenes/Login/Login.js
@@ -1,14 +1,48 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+import { Text, View, TextInput, Image, TouchableHighlight } from "react-native";
 import PropTypes from "prop-types";
 import { styles } from "./styles";
+import NovaButton from "../../components/UI/NovaButton/";
 
-const Login = () => (
-  <View>
-    <Text>Login Scene</Text>
-  </View>
-);
+import { colors, typography } from "../../config/styles.js";
+const { black, mediumGrey, nearBlack, green, red } = colors;
+const { fontMain } = typography;
+class Login extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={{ height: 90, width: 90, marginTop: 20 }}
+          source={require("../../assets/images/NLogo.png")}
+          resizeMode="contain"
+        />
+        <Image
+          style={{ height: 50, width: 150, marginTop: 20 }}
+          source={require("../../assets/images/NOVAheader.png")}
+          resizeMode="contain"
+        />
 
-// Login.propTypes = {};
+        <TextInput value="Email Address" style={styles.input} />
+        <TextInput value="Password" style={styles.input} />
+        <NovaButton title="enter" color={red} />
+        <TouchableHighlight>
+          <Text
+            style={{
+              color: "#ffffff",
+              margin: 20,
+
+              fontFamily: fontMain
+            }}
+          >
+            New User? Create Account
+          </Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+// LoginContainer.propTypes = {};
 
 export default Login;
--- js/scenes/Login/LoginContainer.js
+++ js/scenes/Login/LoginContainer.js
@@ -3,11 +3,18 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Login from "./Login";
-
+import { colors, typography } from "../../config/styles.js";
+const { black, mediumGrey, nearBlack, green, red } = colors;
+const { fontMain } = typography;
 class LoginContainer extends Component {
   static route = {
     navigationBar: {
-      title: "Login"
+      title: "Login",
+      backgroundColor: nearBlack,
+      titleStyle: {
+        color: "#ffffff",
+        fontFamily: fontMain
+      }
     }
   };
 
