ca779d803cbd19c7701a20eb6a0bffde016293f1
michaelujkim <michaelujkim@gmail.com>
2018-03-15 15:41:06 -0700

    second round of feedback incorporated

--- js/scenes/Login/LoginContainer.js
+++ js/scenes/Login/LoginContainer.js
@@ -1,7 +1,5 @@
-// https://projects.invisionapp.com/share/GRFWI99HZKW#/screens/279885482
-
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+
 import Login from "./Login";
 import { colors, typography } from "../../config/styles.js";
 const { black, mediumGrey, nearBlack, green, red } = colors;
@@ -22,7 +20,5 @@ class LoginContainer extends Component {
     return <Login />;
   }
 }
-
-// LoginContainer.propTypes = {};
 
 export default LoginContainer;
