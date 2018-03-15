7d23cd83ac77f509a8425f5bd1586f4bbe25bad1
michaelujkim <michaelujkim@gmail.com>
2018-03-15 15:41:06 -0700

    second round of feedback incorporated

--- js/scenes/Login/Login.js
+++ js/scenes/Login/Login.js
@@ -1,37 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Text, View, TextInput, Image, TouchableHighlight } from "react-native";
-import PropTypes from "prop-types";
+
 import { styles } from "./styles";
 import NovaButton from "../../components/UI/NovaButton/";
 import NovaHR from "../../components/UI/NovaHR/";
 import { colors, typography } from "../../config/styles.js";
 const { black, mediumGrey, nearBlack, green, red } = colors;
 const { fontMain } = typography;
-class Login extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image
-          style={styles.logo}
-          source={require("../../assets/images/NLogo.png")}
-          resizeMode="contain"
-        />
-        <Image
-          style={styles.header}
-          source={require("../../assets/images/NOVAheader.png")}
-          resizeMode="contain"
-        />
-        <NovaHR color={green} />
-        <TextInput value="Email Address" style={styles.input} />
-        <TextInput value="Password" style={styles.input} />
-        <NovaButton title="ENTER" color={red} />
-        <TouchableHighlight>
-          <Text style={styles.newuser}>New User? Create Account</Text>
-        </TouchableHighlight>
-      </View>
-    );
-  }
-}
+const Login = () => (
+  <View style={styles.container}>
+    <Image
+      style={styles.logo}
+      source={require("../../assets/images/NLogo.png")}
+      resizeMode="contain"
+    />
+    <Image
+      style={styles.header}
+      source={require("../../assets/images/NOVAheader.png")}
+      resizeMode="contain"
+    />
+    <NovaHR color={green} />
+    <TextInput value="Email Address" style={styles.input} />
+    <TextInput value="Password" style={styles.input} />
+    <NovaButton title="ENTER" color={red} />
+    <TouchableHighlight>
+      <Text style={styles.newuser}>New User? Create Account</Text>
+    </TouchableHighlight>
+  </View>
+);
 
 export default Login;
