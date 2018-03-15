8c978e4ff08ed9b9675bd3eff30fefdf412890c3
michaelujkim <michaelujkim@gmail.com>
2018-03-15 13:37:22 -0700

    login page set up done

--- js/scenes/Login/styles.js
+++ js/scenes/Login/styles.js
@@ -1,3 +1,23 @@
 import { StyleSheet } from "react-native";
-
-export const styles = StyleSheet.create({});
+import { colors, typography } from "../../config/styles.js";
+const { black, mediumGrey, nearBlack, green, red } = colors;
+const { fontMain } = typography;
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: black
+  },
+  input: {
+    height: 45,
+    width: 200,
+    backgroundColor: "#ffffff",
+    borderWidth: 1,
+    marginTop: 20,
+    color: nearBlack,
+    paddingLeft: 10,
+    fontFamily: fontMain,
+    borderRadius: 3
+  }
+});
