d0565313aceda5ea82c98456ab2a583675d3d6d7
Late Worm <travis.rempel@gmail.com>
2018-03-16 11:28:50 -0700

    Modify csutom heading elements to accept custom styles.

--- js/components/UI/NovaButton/NovaButton.js
+++ js/components/UI/NovaButton/NovaButton.js
@@ -6,10 +6,10 @@ import { styles } from "./styles";
 // Button component
 // You can find an example implementation in the UserAccountCreate component
 
-const NovaButton = ({ title, color, onPress }) => (
+const NovaButton = ({ title, color, style, onPress }) => (
   <View style={styles.novaButtonOuterWrapper}>
     <TouchableHighlight
-      style={[styles.novaButtonTouchable, { backgroundColor: color }]}
+      style={[styles.novaButtonTouchable, style, { backgroundColor: color }]}
       onPress={onPress}
     >
       <View style={styles.novaButtonInnerWrapper}>
@@ -22,7 +22,8 @@ const NovaButton = ({ title, color, onPress }) => (
 NovaButton.propTypes = {
   title: PropTypes.string.isRequired,
   onPress: PropTypes.func.isRequired,
-  color: PropTypes.string.isRequired
+  color: PropTypes.string.isRequired,
+  style: PropTypes.object
 };
 
 export default NovaButton;
