a9e49704e9d843c52ca68bf34e4d32dc199ac913
Late Worm <travis.rempel@gmail.com>
2018-03-16 10:35:31 -0700

    Create CompaniesSearch scene.

---
+++ js/scenes/CompaniesSearch/CompaniesSearch.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View } from "react-native";
+import { styles } from "./styles";
+
+// import Nova UI components:
+
+import NovaImperative from "../../components/UI/NovaImperative";
+
+const CompaniesSearch = () => (
+  <View>
+    <NovaImperative
+      color="black"
+      title="Search for Your Industry of Interest"
+      style={styles.blah}
+    />
+  </View>
+);
+
+export default CompaniesSearch;
