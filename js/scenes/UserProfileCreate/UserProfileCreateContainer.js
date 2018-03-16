30310b6bbd3ec0c0dbb64ddbbe2f7817018f5989
michaelujkim <michaelujkim@gmail.com>
2018-03-16 10:36:55 -0700

    create profile markup and css done

--- js/scenes/UserProfileCreate/UserProfileCreateContainer.js
+++ js/scenes/UserProfileCreate/UserProfileCreateContainer.js
@@ -3,11 +3,19 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import UserProfileCreate from "./UserProfileCreate";
+import { colors, typography } from "../../config/styles.js";
+const { black, mediumGrey, nearBlack, green, red } = colors;
+const { fontMain } = typography;
 
 class UserProfileCreateContainer extends Component {
   static route = {
     navigationBar: {
-      title: "UserProfileCreate"
+      title: "Profile",
+      backgroundColor: nearBlack,
+      titleStyle: {
+        color: "#ffffff",
+        fontFamily: fontMain
+      }
     }
   };
 
--- js/scenes/UserProfileCreate/styles.js
+++ js/scenes/UserProfileCreate/styles.js
@@ -1,3 +1,40 @@
 import { StyleSheet } from "react-native";
+import { colors, typography } from "../../config/styles.js";
+const { black, mediumGrey, nearBlack, green, red } = colors;
+const { fontMain } = typography;
+export const styles = StyleSheet.create({
+  header: {
+    fontFamily: fontMain,
+    fontSize: 16,
+    marginLeft: 15,
+    paddingTop: 15,
+    fontWeight: "bold"
+  },
+  input: {
+    height: 100,
+    width: 290,
+    backgroundColor: mediumGrey,
 
-export const styles = StyleSheet.create({});
+    marginTop: 10,
+    color: nearBlack,
+    padding: 7,
+    fontFamily: fontMain,
+    borderRadius: 3
+  },
+  textInputContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  logo: {
+    marginTop: 10,
+    height: 17,
+    width: 17,
+    marginLeft: 8
+  },
+  global: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center"
+  }
+});
