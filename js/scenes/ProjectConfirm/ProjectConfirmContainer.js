c01a4cbc3425e8804193b7fbda88739e14442a28
michaelujkim <michaelujkim@gmail.com>
2018-03-16 11:39:57 -0700

    css for project Confirm page done

--- js/scenes/ProjectConfirm/ProjectConfirmContainer.js
+++ js/scenes/ProjectConfirm/ProjectConfirmContainer.js
@@ -3,11 +3,19 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import ProjectConfirm from "./ProjectConfirm";
+import { colors, typography } from "../../config/styles.js";
+const { black, mediumGrey, nearBlack, green, red } = colors;
+const { fontMain } = typography;
 
 class ProjectConfirmContainer extends Component {
   static route = {
     navigationBar: {
-      title: "ProjectConfirm"
+      title: "Start Project",
+      backgroundColor: nearBlack,
+      titleStyle: {
+        color: "#ffffff",
+        fontFamily: fontMain
+      }
     }
   };
 
