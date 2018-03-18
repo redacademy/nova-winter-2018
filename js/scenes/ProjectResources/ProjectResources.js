69efe3fd4dce7fcd2b0a75d247fc7f8ae75a459e
Late Worm <travis.rempel@gmail.com>
2018-03-18 16:59:29 -0700

    Add a video player with play/pause/fullscreen controls and progress bar.

--- js/scenes/ProjectResources/ProjectResources.js
+++ js/scenes/ProjectResources/ProjectResources.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Text, View } from "react-native";
-
 import { colors } from "../../config/styles";
 const { red } = colors;
+import VideoPlayer from "react-native-video-player";
+import { styles } from "./styles";
 
 // import Nova UI components:
 import NovaH2 from "../../components/UI/NovaH2";
@@ -13,6 +14,17 @@ const ProjectResources = () => (
     <Text>
       Carefully watch each video in sequence to prepare for your test.
     </Text>
+    <View style={styles.videoContainer}>
+      <VideoPlayer
+        // https://github.com/cornedor/react-native-video-player/blob/master/example/app/index.js
+        video={{
+          uri:
+            "https://ia801403.us.archive.org/13/items/AboutBan1935/AboutBan1935_512kb.mp4"
+        }} // Can be a URL or a local file.
+        videoWidth={320}
+        videoHeight={240}
+      />
+    </View>
   </View>
 );
 
