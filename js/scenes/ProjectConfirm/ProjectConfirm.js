3f1eb87d108f6120b2d35327ee3a1bb10e50af97
michaelujkim <michaelujkim@gmail.com>
2018-03-19 11:31:35 -0700

    css and markup for project confirm

--- js/scenes/ProjectConfirm/ProjectConfirm.js
+++ js/scenes/ProjectConfirm/ProjectConfirm.js
@@ -2,16 +2,18 @@ import React from "react";
 import { Text, View } from "react-native";
 
 import { colors } from "../../config/styles";
-const { red } = colors;
 
+import { styles } from "./styles";
 // import Nova UI components:
 import NovaH1 from "../../components/UI/NovaH1";
 import NovaButton from "../../components/UI/NovaButton";
-
+const { red } = colors;
 const ProjectConfirm = () => (
-  <View>
+  <View style={styles.container}>
     <NovaH1 color={red} title="Are You Ready?" />
-    <Text>Make sure you are set up in an appropriate work environment</Text>
+    <Text style={styles.message}>
+      Make sure you are set up in an appropriate work environment
+    </Text>
     <NovaButton title="BEGIN" color={red} onPress={null} />
   </View>
 );
