449d3e706c4174124d5d5ab4048ab06939596157
Scott <scottdlivingstone@gmail.com>
2018-03-19 10:04:23 -0700

    Write function to check persistent user status in splash screen when app loads

--- js/scenes/Splash/SplashContainer.js
+++ js/scenes/Splash/SplashContainer.js
@@ -1,7 +1,15 @@
 // https://projects.invisionapp.com/share/GRFWI99HZKW#/screens
 
 import React, { Component } from "react";
+import { AsyncStorage } from "react-native";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
+
+import {
+  setAuthenticationStatus,
+  setUserState,
+  userLoading
+} from "../../redux/modules/auth";
 import Splash from "./Splash";
 
 class SplashContainer extends Component {
@@ -11,11 +19,28 @@ class SplashContainer extends Component {
     }
   };
 
+  componentDidMount() {
+    this.props.dispatch(userLoading(true));
+    AsyncStorage.getItem("USER", (err, result) => {
+      if (result) {
+        this.props.dispatch(setUserState(JSON.parse(result)));
+        this.props.dispatch(setAuthenticationStatus(true));
+      }
+      this.props.dispatch(userLoading(false));
+    });
+  }
+
   render() {
     return <Splash />;
   }
 }
 
-// SplashContainer.propTypes = {};
+SplashContainer.propTypes = {
+  dispatch: PropTypes.func.isRequired
+};
 
-export default SplashContainer;
+const mapStateToProps = state => ({
+  state
+});
+
+export default connect(mapStateToProps)(SplashContainer);
