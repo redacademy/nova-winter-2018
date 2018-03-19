3869c8951120cfd18e21723cfd734a45c234441f
Sstobo <seanstobo@gmail.com>
2018-03-19 11:43:04 -0700

    User reducer, Company reducer, firebaseHelpers all completed

---
+++ js/api/firebaseHelpers.js
@@ -0,0 +1,95 @@
+import {
+  database
+} from "./firebase";
+
+export function writeNewCompanyData(companyID, name, contact_name, contact_email, company_info, location) {
+  database
+    .collection("companys")
+    .doc(companyID)
+    .set({
+      name: name,
+      contact_name: contact_name,
+      contact_email: contact_email,
+      company_info: company_info,
+      location: location
+    })
+    .then(function () {
+      console.log("Company data written!");
+    });
+}
+
+export function writeNewCompanyProjects(companyID, project_name, deliverables, project_info, start_time, video1, video2, video3) {
+  database
+    .collection("companys")
+    .doc(companyID)
+    .collection("projects")
+    .set({
+      project_name: project_name,
+      deliverables: deliverables,
+      project_info: project_info,
+      start_time: start_time,
+      video1: video1,
+      video2: video2,
+      video3: video3
+    })
+    .then(function () {
+      console.log("Company data written!");
+    });
+}
+
+
+export function writeNewUserData(userID, name, email) {
+  database
+    .collection("users")
+    .doc(userID)
+    .set({
+      name: "Mike",
+      email: "mike@mike.com"
+    })
+    .then(function () {
+      console.log("User data written!");
+    });
+}
+export function writeNewUserInformation(userID, bio, experience, projects) {
+  database
+    .collection("users")
+    .doc(userID)
+    .set({
+      bio: bio,
+      experience: experience,
+      projects: projects
+    })
+    .then(function () {
+      console.log("User info written!");
+    });
+}
+
+export function writeNewUserLocation(userID, location) {
+  database
+    .collection("users")
+    .doc(userID)
+    .set({
+      location: location
+    })
+    .then(function () {
+      console.log("User location written!");
+    });
+}
+export function writeNewUserTests(
+  userID,
+  currentTest,
+  testStartTime,
+  testScore
+) {
+  database
+    .collection("users")
+    .doc(userID)
+    .set({
+      currentTest: currentTest,
+      testStartTime: testStartTime,
+      testScore: testScore
+    })
+    .then(function () {
+      console.log("User location written!");
+    });
+}
--- js/redux/reducers.js
+++ js/redux/reducers.js
@@ -1,10 +1,18 @@
-import { combineReducers } from "redux";
+import {
+  combineReducers
+} from "redux";
 import authReducer from "./modules/auth";
-import { NavigationReducer } from "@expo/ex-navigation";
+import companyReducer from "./modules/companyReducer";
+import userReducer from "./modules/userReducer";
+import {
+  NavigationReducer
+} from "@expo/ex-navigation";
 
 const rootReducer = combineReducers({
   navigation: NavigationReducer,
-  auth: authReducer
+  auth: authReducer,
+  company: companyReducer,
+  user: userReducer
 });
 
 export default rootReducer;
