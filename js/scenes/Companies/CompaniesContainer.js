e31db39955819cb809b33d9181d54ce4994b4e13
Sstobo <seanstobo@gmail.com>
2018-03-20 15:42:24 -0700

    User profile page styled and pulling

--- js/scenes/Companies/CompaniesContainer.js
+++ js/scenes/Companies/CompaniesContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Companies from "./Companies";
-
 import { connect } from "react-redux";
 import { getAllCompanys } from "../../redux/modules/companyReducer";
 
