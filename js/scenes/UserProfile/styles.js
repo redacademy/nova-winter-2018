02d24b8cd3dc8042975956ea5a9553c0fb556f4f
Sstobo <seanstobo@gmail.com>
2018-03-20 15:42:24 -0700

    User profile page styled and pulling

--- js/scenes/UserProfile/styles.js
+++ js/scenes/UserProfile/styles.js
@@ -1,3 +1,36 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  h1: {
+    alignSelf: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 18,
+    marginTop: 12
+  },
+  h2: {
+    alignSelf: "center",
+    fontWeight: "200",
+    fontSize: 16
+  },
+  headline: {
+    color: "#E17B61",
+    paddingBottom: 12,
+    paddingTop: 12,
+    fontSize: 17,
+    letterSpacing: 0.2
+  },
+  text: {
+    alignSelf: "center",
+    lineHeight: 22,
+    fontSize: 14,
+    fontWeight: "100",
+    letterSpacing: 0.1
+  },
+  viewbox: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#E17B61",
+    paddingBottom: 22,
+    paddingTop: 12
+  }
+});
